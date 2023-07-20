0a7e9c364fa4d49d949b35e6572368a1481f8a19
sofi3a <140101681+sofi3a@users.noreply.github.com>
2023-07-20 13:04:40 -0400

    Add files via upload

---
+++ sofia /intro -sofia/Sofia/MP2(new) - sofia/MP2(new) - sofia/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MP2(new) - sofia
+//
+//  Created by SMART Scholars on 7/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var answerDisplay: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sofia /intro -sofia/Sofia/Sofia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sofia
+//
+//  Created by SMART Scholars on 7/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fact1: UILabel!
+    
+    @IBOutlet weak var fact2: UILabel!
+    
+    @IBOutlet weak var fact3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
